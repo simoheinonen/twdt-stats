414953f6fbeb3a6a3410a3b1bcfd13b3c1fccfc3
Simo Heinonen <simo@dudgeon.fi>
2019-08-06 23:58:19 +0300

    Week 3

--- index.php
+++ index.php
@@ -2,23 +2,29 @@
 
 require_once 'vendor/autoload.php';
 
-$a = file_get_contents('twdtd-w4.txt');
+
+$data = [];
+
+for ($week = 3; $week <= 4; $week++) {
+
+$a = file_get_contents('twdtd-w'.$week.'.txt');
 
 $r = '/^[ ][|]\s+(?P<stars>\d+)\s+\s(?P<names>.*?)\s+(<MVP>){0,}\s+(?P<kills>\d+)[ ][|]\s+(?P<deaths>\d+)\s+[|]/m';
 
 preg_match_all($r, $a, $mat);
 
-$data = [];
-
 foreach ($mat['names'] as $i => $name) {
   $data[$name]['name'] = $name;
-  @$data[$name]['kills'] += $mat['kills'][$i];
-  @$data[$name]['deaths'] += $mat['deaths'][$i];
+  $data[$name]['w' . $week][] = ['k' => $mat['kills'][$i], 'd' => $mat['deaths'][$i]];
+ }
 }
+
+//var_dump($data); exit;
+/*
 usort($data , function($a, $b){
 return $a['kills'] < $b['kills'];
 });
-
+*/
 
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
