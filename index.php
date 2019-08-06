f0f8782036ec666ac9c1831f451fd843e9895b0c
Simo Heinonen <simo@dudgeon.fi>
2019-08-07 00:20:17 +0300

    Week 1 and 2

--- index.php
+++ index.php
@@ -5,27 +5,34 @@ require_once 'vendor/autoload.php';
 
 $data = [];
 
-for ($week = 3; $week <= 4; $week++) {
+for ($week = 1; $week <= 4; $week++) {
 
 $a = file_get_contents('twdtd-w'.$week.'.txt');
 
-$r = '/^[ ][|]\s+(?P<stars>\d+)\s+\s(?P<names>.*?)\s+(<MVP>){0,}\s+(?P<kills>\d+)[ ][|]\s+(?P<deaths>\d+)\s+[|]/m';
+$week1r = '/^[ ][|]\s+(?P<names>.*?)\s+(<MVP>){0,}\s+(?P<stars>\d+)[ ][|]\s+(?P<kills>\d+)\s+[|]\s+(?P<deaths>\d+)\s+[|]/m';
 
+$r = '/^[ ][|]\s+(?P<stars>\d+)\s+(?P<names>.*?)\s+(<MVP>){0,}\s+(?P<kills>\d+)[ ][|]\s+(?P<deaths>\d+)\s+[|]/m';
+
+if ($week === 1) {
+preg_match_all($week1r, $a, $mat);
+} else {
 preg_match_all($r, $a, $mat);
-
+}
 foreach ($mat['names'] as $i => $name) {
   $data[$name]['name'] = $name;
   $data[$name]['w' . $week][] = ['k' => $mat['kills'][$i], 'd' => $mat['deaths'][$i]];
  }
 }
 
-//var_dump($data); exit;
-/*
-usort($data , function($a, $b){
-return $a['kills'] < $b['kills'];
-});
-*/
 
+// lol
+foreach ($data as $name => $da) {
+ if (stristr($name, '/') !== false) {
+unset($data[$name]);
+}
+}
+
+var_dump($data); exit;
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
